680e8961002dba01bca9120e7a5b1e47170111a4
Darlyn30 <zomberkawaiimusic@gmail.com>
2026-08-25 23:34:55 -0400

    fix: correcion de variables de entorno

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,7 +1,6 @@
 import { Sequelize } from "sequelize";
-process.loadEnvFile();
+// process.loadEnvFile();
 
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT
+export const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'mysql'
 });
