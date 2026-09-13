33cded53b09ffefb6ca329d7e4c04b82b831d5d8
Darlyn Olivo <105079724+Darlyn30@users.noreply.github.com>
2026-09-13 07:38:53 +0200

    Update dbConfig.js

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,12 +2,12 @@ import { Sequelize } from "sequelize";
 // process.loadEnvFile();
 
 // para produccion
-// export const sequelize = new Sequelize(process.env.DB_URL, {
-//   dialect: 'mysql'
-// });
+export const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'mysql'
+});
 
 // para desarrollo
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT
-});
+// export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//   host: process.env.DB_HOST,
+//   dialect: process.env.DB_DIALECT
+// });
